d5c5ea3688b4e9dc83ea73619436af67a90cccf3
Bárbara Araújo Paim <barbaraaraujopaim99@gmail.com>
2021-05-06 13:53:06 -0300

    Initial Commit

---
+++ Shared/TimerEx1App.swift
@@ -0,0 +1,17 @@
+//
+//  TimerEx1App.swift
+//  Shared
+//
+//  Created by Bárbara Araújo Paim  on 06/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerEx1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
